927aa8bd669050a0eb824bb91ef9e15fe404321c
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-17 22:16:58 -0500

    initial file structure

---
+++ server.js
@@ -0,0 +1,41 @@
+//global variables
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+// set up variable for connection and sql session
+const sequelize = require('./config/connection');
+const SqlStore = require('connect-session-sequelize')(session.Store);
+
+// create variable to store session sql
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  
+  app.use(session(sess));
+
+// create variable for handlebars
+const hbs = exphbs.create({});
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+}); 

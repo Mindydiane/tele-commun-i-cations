8b99447a813153799287f82a25e650fe161728aa
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-18 22:42:41 -0500

    add post js and to handlebars dashboard

---
+++ public/js/add-post.js
@@ -0,0 +1,31 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  console.log("I've been submeeeteeeed");
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_text = document.querySelector('textarea[name="post-text"]').value;
+
+  console.log(post_text);
+
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      title: title,
+      post_text: post_text,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);

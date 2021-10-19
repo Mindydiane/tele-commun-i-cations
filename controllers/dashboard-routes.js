dba4e20ebb75114a4a0a9022d3e125237a527627
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-18 21:48:38 -0500

    get all posts for dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,4 +3,45 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+// get all posts for dashboard
+router.get('/', withAuth, (req, res) => {
+    console.log(req.session.user_id);
+    Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        console.log("Here are my posts:");
+        console.log(posts);
+        res.render('dashboard', { posts, loggedIn: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+    // get post by id
+
 module.exports = router;
